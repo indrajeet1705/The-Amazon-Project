b1a676353bd7121c9d5a4e5664ffec9eb123d6b0
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2024-12-23 12:42:36 +0530

    fixed price formating and redio sclectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "./util/money.js"; 
 
 
 let cartSummaryHtml='';
@@ -33,7 +33,7 @@ cartSummaryHtml+=
                  ${matchingItem.name}
                 </div>
                 <div class="product-price">
-                ${(matchingItem.priceCents/100).toFixed(2)}
+                ${formatCurrency(matchingItem.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -55,7 +55,7 @@ cartSummaryHtml+=
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -68,7 +68,7 @@ cartSummaryHtml+=
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -81,7 +81,7 @@ cartSummaryHtml+=
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
